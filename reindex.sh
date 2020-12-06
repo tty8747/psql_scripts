803d64d334295d04d8379e401c8e50a9ab955470
Sergey Chulanov <tty8747@gmail.com>
2020-12-07 02:13:27 +0300

    Added reindex

--- reindex.sh
+++ reindex.sh
@@ -4,8 +4,18 @@ set -e
 
 source ./shared_func.sh
 
-getDBnames
+checkVar "$(getEnvVars)"
+CheckDependences s-nail
 
-bash ./unsetvars.sh
+for db in $(getDBnames)
+  do
+      if log="$(reindex "$db")"
+      then
+        sendEmail "$(makeid $db)" "$log" "reindex" true
+    else
+        sendEmail "$(makeid $db)" "$log" "reindex" false
+    fi
+  done
 
+unsetVar "$(getEnvVars)"
 exit 0
