69a123f3323c122dd2fa85b91d866da630c4e7a6
Sergey <tty8747@gmail.com>
2021-04-02 14:16:04 +0300

    Update shared_func.sh

--- shared_func.sh
+++ shared_func.sh
@@ -68,7 +68,7 @@ function getDBnames () {
     then
       echo $dblist
     else
-    error_exit "Error:$?, line: $LINENO. There isn't databases."
+    error_exit "Error:$?, line: $LINENO. There aren't databases."
   fi
 }
 
